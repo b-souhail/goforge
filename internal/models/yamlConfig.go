682e4d5720e9e57bb9c570803bdee2152e0a86a5
b-souhail <b.souhail.dev@gmail.com>
2026-03-13 17:00:29 +0100

    feat(cmd):resolve issues and enable -m

--- internal/models/yamlConfig.go
+++ internal/models/yamlConfig.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
 type Layer struct {
 	Name string   `yaml:"name"`
 	Dirs []string `yaml:"dirs,omitempty"`
@@ -12,6 +18,20 @@ type Config struct {
 	Modules      string  `yaml:"modules"`
 	Layers       []Layer `yaml:"layers"`
 	Plugins      string  `yaml:"plugins,omitempty"`
+}
+
+func (c *Config) ReadYaml(configFile string) (Config, error) {
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
 }
 
 //   - name: str
