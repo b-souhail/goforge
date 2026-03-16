47092644ce1f2092f11391d0cf085d28066a3b39
b-souhail <b.souhail.dev@gmail.com>
2026-03-16 03:08:21 +0100

    fix: feat(setup): clean setup command

---
+++ internal/generate/scaffold.go
@@ -0,0 +1,15 @@
+package generate
+
+import (
+	"embed"
+	"fmt"
+	"goforge/internal/models"
+)
+
+//go:embddded temdplates/clean
+var templatesFS embed.FS
+
+func ScaffoldConfig(cfg models.Config) error {
+    fmt.Println(cfg.Name)
+	return nil
+}
